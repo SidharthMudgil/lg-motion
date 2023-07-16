fc5a4cfe860e6eb538cfe9c124d20509407e350d
SidharthMudgil <68889544+SidharthMudgil@users.noreply.github.com>
2023-07-16 14:36:27 +0530

    navigation added

--- app/src/main/java/com/sidharth/lg_motion/MainActivity.kt
+++ app/src/main/java/com/sidharth/lg_motion/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.sidharth.lg_motion
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.sidharth.lg_motion.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val activityMainBinding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(activityMainBinding.root)
+        setupBottomNavigation()
+    }
+
+    private fun setupBottomNavigation() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+        navHostFragment?.findNavController()?.apply {
+            activityMainBinding.bottomNavigationView.setupWithNavController(this)
+        }
     }
 }
