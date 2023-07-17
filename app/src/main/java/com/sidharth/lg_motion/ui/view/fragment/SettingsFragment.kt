6433fe033d3d686f0721fdb255f5e6bcd2cee66e
SidharthMudgil <68889544+SidharthMudgil@users.noreply.github.com>
2023-07-17 18:05:17 +0530

    initial ui for SettingsFragment

--- app/src/main/java/com/sidharth/lg_motion/ui/view/fragment/SettingsFragment.kt
+++ app/src/main/java/com/sidharth/lg_motion/ui/view/fragment/SettingsFragment.kt
@@ -1,23 +1,12 @@
 package com.sidharth.lg_motion.ui.view.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.preference.PreferenceFragmentCompat
 import com.sidharth.lg_motion.R
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
     }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_settings, container, false)
-    }
-
 }
