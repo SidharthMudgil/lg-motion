591c1a8e570b348a3b284d20f6a4a509a1795911
SidharthMudgil <68889544+SidharthMudgil@users.noreply.github.com>
2023-07-19 17:52:12 +0530

    saferargs added for navigation

--- app/src/main/java/com/sidharth/lg_motion/ui/view/fragment/SettingsFragment.kt
+++ app/src/main/java/com/sidharth/lg_motion/ui/view/fragment/SettingsFragment.kt
@@ -3,6 +3,7 @@ package com.sidharth.lg_motion.ui.view.fragment
 import android.os.Bundle
 import android.text.InputFilter
 import android.text.InputType
+import androidx.navigation.findNavController
 import androidx.preference.EditTextPreference
 import androidx.preference.ListPreference
 import androidx.preference.Preference
@@ -18,27 +19,27 @@ class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
 
-        val username = findPreference<EditTextPreference>("username")
-        val password = findPreference<EditTextPreference>("password")
-        val ip = findPreference<EditTextPreference>("ip")
-        val port = findPreference<EditTextPreference>("port")
-        val totalScreens = findPreference<SeekBarPreference>("screens")
-        val autoConnect = findPreference<SwitchPreferenceCompat>("auto_connect")
-        val clearKml = findPreference<Preference>("clear_kml")
-        val setRefresh = findPreference<Preference>("set_refresh")
-        val resetRefresh = findPreference<Preference>("reset_refresh")
-        val relaunch = findPreference<Preference>("relaunch")
-        val restart = findPreference<Preference>("restart")
-        val shutdown = findPreference<Preference>("shutdown")
-        val mapStyle = findPreference<ListPreference>("map_style")
-        val about = findPreference<Preference>("about")
-        val openSourceLicense = findPreference<Preference>("opensource_license")
-        val privacyPolicy = findPreference<Preference>("privacy_policy")
-        val appVersion = findPreference<Preference>("app_version")
+        val usernamePreference = findPreference<EditTextPreference>("username")
+        val passwordPreference = findPreference<EditTextPreference>("password")
+        val ipPreference = findPreference<EditTextPreference>("ip")
+        val portPreference = findPreference<EditTextPreference>("port")
+        val totalScreensPreference = findPreference<SeekBarPreference>("screens")
+        val autoConnectPreference = findPreference<SwitchPreferenceCompat>("auto_connect")
+        val clearKmlPreference = findPreference<Preference>("clear_kml")
+        val setRefreshPreference = findPreference<Preference>("set_refresh")
+        val resetRefreshPreference = findPreference<Preference>("reset_refresh")
+        val relaunchPreference = findPreference<Preference>("relaunch")
+        val restartPreference = findPreference<Preference>("restart")
+        val shutdownPreference = findPreference<Preference>("shutdown")
+        val mapStylePreference = findPreference<ListPreference>("map_style")
+        val aboutPreference = findPreference<Preference>("about")
+        val openSourceLicensePreference = findPreference<Preference>("opensource_license")
+        val privacyPolicyPreference = findPreference<Preference>("privacy_policy")
+        val appVersionPreference = findPreference<Preference>("app_version")
 
         val inputFilter = InputFilter.LengthFilter(30)
 
-        username?.apply {
+        usernamePreference?.apply {
             setOnBindEditTextListener { editText ->
                 editText.isSingleLine = true
                 editText.inputType = InputType.TYPE_CLASS_TEXT
@@ -46,7 +47,7 @@ class SettingsFragment : PreferenceFragmentCompat() {
             }
         }
 
-        password?.apply {
+        passwordPreference?.apply {
             setOnBindEditTextListener { editText ->
                 editText.isSingleLine = true
                 editText.inputType = InputType.TYPE_TEXT_VARIATION_PASSWORD
@@ -54,7 +55,7 @@ class SettingsFragment : PreferenceFragmentCompat() {
             }
         }
 
-        ip?.apply {
+        ipPreference?.apply {
             setOnBindEditTextListener { editText ->
                 editText.isSingleLine = true
                 editText.inputType = InputType.TYPE_CLASS_TEXT
@@ -62,7 +63,7 @@ class SettingsFragment : PreferenceFragmentCompat() {
             }
         }
 
-        port?.apply {
+        portPreference?.apply {
             setOnBindEditTextListener { editText ->
                 editText.isSingleLine = true
                 editText.inputType = InputType.TYPE_CLASS_NUMBER
@@ -70,7 +71,7 @@ class SettingsFragment : PreferenceFragmentCompat() {
             }
         }
 
-        appVersion?.setOnPreferenceClickListener {
+        appVersionPreference?.setOnPreferenceClickListener {
             ToastUtil.showToast(
                 requireContext(),
                 "version ${
@@ -80,6 +81,24 @@ class SettingsFragment : PreferenceFragmentCompat() {
                     ).versionName
                 }"
             )
+            true
+        }
+
+        aboutPreference?.setOnPreferenceClickListener {
+            val action = SettingsFragmentDirections.actionSettingsFragmentToAboutFragment()
+            view?.findNavController()?.navigate(action)
+            true
+        }
+
+        openSourceLicensePreference?.setOnPreferenceClickListener {
+            val action = SettingsFragmentDirections.actionSettingsFragmentToOpenSourceLicenseFragment()
+            view?.findNavController()?.navigate(action)
+            true
+        }
+
+        privacyPolicyPreference?.setOnPreferenceClickListener {
+            val action = SettingsFragmentDirections.actionSettingsFragmentToPrivacyPolicyFragment()
+            view?.findNavController()?.navigate(action)
             true
         }
     }
