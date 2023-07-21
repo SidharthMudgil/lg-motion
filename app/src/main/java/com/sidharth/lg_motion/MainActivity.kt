8b978183ed4abfef93e8e9665295d60e86f47c00
SidharthMudgil <68889544+SidharthMudgil@users.noreply.github.com>
2023-07-21 10:01:42 +0530

    landscape and tablet resource qualifiers added for activity_main

--- app/src/main/java/com/sidharth/lg_motion/MainActivity.kt
+++ app/src/main/java/com/sidharth/lg_motion/MainActivity.kt
@@ -2,6 +2,7 @@ package com.sidharth.lg_motion
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.sidharth.lg_motion.databinding.ActivityMainBinding
@@ -10,17 +11,25 @@ class MainActivity : AppCompatActivity() {
     private val activityMainBinding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+    private var navHostFragment: Fragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activityMainBinding.root)
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
         setupBottomNavigation()
+        setupRailViewNavigation()
     }
 
     private fun setupBottomNavigation() {
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
         navHostFragment?.findNavController()?.apply {
-            activityMainBinding.bottomNavigationView.setupWithNavController(this)
+            activityMainBinding.bottomNavigationView?.setupWithNavController(this)
+        }
+    }
+
+    private fun setupRailViewNavigation() {
+        navHostFragment?.findNavController()?.apply {
+            activityMainBinding.navigationRailView?.setupWithNavController(this)
         }
     }
 }
