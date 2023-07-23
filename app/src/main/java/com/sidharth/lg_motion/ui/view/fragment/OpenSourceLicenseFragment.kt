3c6677fdf16b93abce8a15e13ab880b765663865
SidharthMudgil <68889544+SidharthMudgil@users.noreply.github.com>
2023-07-23 21:59:47 +0530

    final logic & ui for OpenSourceLicenseFragment

--- app/src/main/java/com/sidharth/lg_motion/ui/view/fragment/OpenSourceLicenseFragment.kt
+++ app/src/main/java/com/sidharth/lg_motion/ui/view/fragment/OpenSourceLicenseFragment.kt
@@ -1,26 +1,32 @@
 package com.sidharth.lg_motion.ui.view.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
 import com.sidharth.lg_motion.databinding.FragmentOpenSourceLicenseBinding
 
+
 class OpenSourceLicenseFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val binding = FragmentOpenSourceLicenseBinding.inflate(inflater)
-
-
-
+        binding.webView.loadUrl("file:///android_asset/license.html")
+        binding.webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView,
+                request: WebResourceRequest
+            ): Boolean {
+                view.loadUrl(request.url.toString())
+                return true
+            }
+        }
         return binding.root
     }
 }
