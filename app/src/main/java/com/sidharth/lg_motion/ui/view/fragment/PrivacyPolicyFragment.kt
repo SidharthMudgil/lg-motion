6323113b66caa1bb9118e0aa86754d7410f1258d
SidharthMudgil <68889544+SidharthMudgil@users.noreply.github.com>
2023-07-23 22:18:26 +0530

    final logic & ui for PrivacyPolicyFragment

--- app/src/main/java/com/sidharth/lg_motion/ui/view/fragment/PrivacyPolicyFragment.kt
+++ app/src/main/java/com/sidharth/lg_motion/ui/view/fragment/PrivacyPolicyFragment.kt
@@ -1,25 +1,31 @@
 package com.sidharth.lg_motion.ui.view.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
 import com.sidharth.lg_motion.databinding.FragmentPrivacyPolicyBinding
 
 class PrivacyPolicyFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val binding = FragmentPrivacyPolicyBinding.inflate(inflater)
-
-
+        binding.webView.loadUrl("file:///android_asset/policy.html")
+        binding.webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView,
+                request: WebResourceRequest
+            ): Boolean {
+                view.loadUrl(request.url.toString())
+                return true
+            }
+        }
         return binding.root
     }
 }
