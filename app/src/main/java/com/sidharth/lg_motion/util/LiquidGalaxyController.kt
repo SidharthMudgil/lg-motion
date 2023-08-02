ce279fb67f39b65462175485fe4be37b5905f477
SidharthMudgil <68889544+SidharthMudgil@users.noreply.github.com>
2023-08-02 12:26:04 +0530

    bug fixes: actions not performing on the lg

--- app/src/main/java/com/sidharth/lg_motion/util/LiquidGalaxyController.kt
+++ app/src/main/java/com/sidharth/lg_motion/util/LiquidGalaxyController.kt
@@ -7,7 +7,6 @@ import com.jcraft.jsch.JSch
 import com.jcraft.jsch.Session
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import java.io.ByteArrayOutputStream
 import java.io.File
 import java.util.Properties
 
@@ -20,12 +19,10 @@ class LiquidGalaxyController(
     private val screens: Int,
 ) {
     enum class State(val key: String) {
-        IDLE("idle"), FLY_TO("idle"), PLANET("idle"), MOVE_NORTH("Up"), MOVE_SOUTH("Down"), MOVE_EAST(
-            "Right"
-        ),
-        MOVE_WEST("Left"), ROTATE_LEFT("ctrl+Left"), ROTATE_RIGHT("ctrl+Right"), ZOOM_IN("equal"), ZOOM_OUT(
-            "minus"
-        ),
+        IDLE("idle"), FLY_TO("idle"), PLANET("idle"),
+        MOVE_NORTH("Up"), MOVE_SOUTH("Down"), MOVE_EAST("Right"), MOVE_WEST("Left"),
+        ROTATE_LEFT("ctrl+Left"), ROTATE_RIGHT("ctrl+Right"),
+        ZOOM_IN("equal"), ZOOM_OUT("minus"),
     }
 
     private var lastState = State.IDLE
@@ -102,14 +99,8 @@ class LiquidGalaxyController(
             try {
                 if (session != null && connected) {
                     val channel = session?.openChannel("exec") as ChannelExec
-                    channel.outputStream = ByteArrayOutputStream()
                     channel.setCommand(command)
                     channel.connect()
-
-                    while (!channel.isConnected) {
-                        Log.d("execute", "channel opening")
-                    }
-
                     channel.disconnect()
                 }
             } catch (e: Exception) {
@@ -136,20 +127,6 @@ class LiquidGalaxyController(
             """.trimMargin()
 
         execute(command)
-    }
-
-
-    //    suspend fun cleanSlaves() {
-//        for (i in 2..screens) {
-//            execute("echo '' > /var/www/html/kml/slave_$i.kml")
-//        }
-//    }
-    suspend fun sendKmlInSlave(slave: Int, kml: String) {
-        execute("echo '$kml' > /var/www/html/kml/slave_$slave.kml")
-    }
-
-    suspend fun sendKml(name: String) {
-        execute("echo '\nhttp://lg1:81/$name.kml' > /var/www/html/kmls.txt")
     }
 
     suspend fun clearKml() {
@@ -200,7 +177,7 @@ class LiquidGalaxyController(
         execute("""echo "playtour=Orbit" > /tmp/query.txt""")
     }
 
-    suspend fun stopOrbit() {
+    private suspend fun stopOrbit() {
         execute("""echo "exittour=true" > /tmp/query.txt""")
     }
 
@@ -230,8 +207,9 @@ class LiquidGalaxyController(
     }
 
     suspend fun relaunch() {
-        for (i in 1..screens) {
-            val command = """RELAUNCH_CMD="\\
+        for (i in screens downTo 1) {
+            val command = """
+                RELAUNCH_CMD="\
                 if [ -f /etc/init/lxdm.conf ]; then
                 export SERVICE=lxdm
                 elif [ -f /etc/init/lightdm.conf ]; then
@@ -239,12 +217,12 @@ class LiquidGalaxyController(
                 else
                 exit 1
                 fi
-                if  [[ ${'$'}(service ${'$'}SERVICE status) =~ 'stop' ]]; then
-                echo $password | sudo -S service ${'$'}{SERVICE} start
+                if  [[ \${'$'}(service \${'$'}SERVICE status) =~ 'stop' ]]; then
+                echo $password | sudo -S service \${'$'}{SERVICE} start
                 else
-                echo $password | sudo -S service ${'$'}{SERVICE} restart
+                echo $password | sudo -S service \${'$'}{SERVICE} restart
                 fi
-                " && sshpass -p $password ssh -x -t lg@lg\$i "${'$'}RELAUNCH_CMD\"""".trimMargin()
+                " && sshpass -p $password ssh -x -t lg@lg$i "${'$'}RELAUNCH_CMD\"""".trimMargin()
 
             execute(""""/home/$username/bin/lg-relaunch" > /home/$username/log.txt""")
             execute(command)
@@ -252,29 +230,36 @@ class LiquidGalaxyController(
     }
 
     suspend fun restart() {
-        for (i in 1..screens) {
+        for (i in screens downTo 1) {
             execute("""sshpass -p $password ssh -t lg$i "echo $password | sudo -S reboot"""")
         }
     }
 
     suspend fun shutdown() {
-        for (i in 1..screens) {
+        for (i in screens downTo 1) {
             execute("""sshpass -p $password ssh -t lg$i "echo $password | sudo -S poweroff"""")
         }
     }
 
     suspend fun performAction(state: State, direction: String?) {
-        val focusWindow = """xdotool windowfocus ${'$'}(xdotool search --name "Google Earth Pro")"""
-        if (state == State.IDLE || (lastState != State.IDLE && lastState != state)) {
-            execute("""$focusWindow keyup ${lastState.key}""")
+        if ((state == State.IDLE) || (lastState != State.IDLE && lastState != state)) {
+            Log.d("execute", "keyup: \"export DISPLAY=:0; xdotool keyup ${lastState.key}\"")
+            execute("export DISPLAY=:0; xdotool keyup ${lastState.key}")
+            lastState = State.IDLE
         }
-        val command = when (state) {
-            State.PLANET -> """$focusWindow && echo 'planet=$direction' > /tmp/query.txt`"""
-            State.FLY_TO -> """$focusWindow && echo 'search=$direction' > /tmp/query.txt"""
-            else -> "$focusWindow keydown ${state.key}"
+        if (state != State.IDLE) {
+            val command = when (state) {
+                State.PLANET -> "echo 'planet=$direction' > /tmp/query.txt"
+                State.FLY_TO -> "echo 'search=$direction' > /tmp/query.txt"
+                else -> "export DISPLAY=:0; xdotool keydown ${state.key}"
+            }
+            Log.d("execute", "keydown: '$command'")
+            execute(command)
+            lastState = when (state) {
+                State.PLANET, State.FLY_TO -> State.IDLE
+                else -> state
+            }
         }
-        execute(command)
-        lastState = state
     }
 
     companion object {
